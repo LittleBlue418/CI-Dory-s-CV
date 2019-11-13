0b0eb3aab7c970ba5ae6c04924b60c1fde39e42e
LittleBlue418 <hollyathomas88@gmail.com>
2019-11-13 11:26:32 +0100

    refined pins + replaced text for meetups

--- assets/JS/maps.js
+++ assets/JS/maps.js
@@ -2,19 +2,18 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
     center: {
-      lat: -26.396688,
-      lng: 174.569519
+      lat: -19.882245,
+      lng: 156.565945
     }
   });
 
   var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   var locations = [
-
-    { lat: -17.864677, lng: 172.650339 },
-    { lat: -33.854341, lng: 151.218449 },
-    { lat: -34.569345, lng: 174.823199 },
-    { lat: -12.368470, lng: 162.361579 }
+    { lat: -12.368470, lng: 162.361579 },
+    { lat:  -17.743699, lng: 158.915511 },
+    { lat:  -20.479164, lng: 151.735670 },
+    { lat: -33.854341, lng: 151.218449 }
   ];
 
   var markers = locations.map(function (location, i) {
